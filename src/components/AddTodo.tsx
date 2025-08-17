31f14cb9363f7e8ff449838b032caa5a1bc67de4
Revanza Firdaus <revanza.firdaus@gmail.com>
2025-08-18 05:04:42 +0700

    feat: Introduce todo add

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,37 @@
+import React, { type FormEvent, useContext, useState } from "react";
+import {Context as TodoContext} from '../context/TodoContext'
+import { addTodo } from "../actions/TodoAction";
+
+
+const AddTodo:React.FC = () => {
+    const {dispatch} = useContext(TodoContext)
+    const [name,setName] = useState('')
+
+
+    const handleAdd = () => {
+        if(!name.trim()){
+            return
+        }
+        
+        dispatch(
+            addTodo({
+                id:Date.now().toString(),
+                name,
+                active:true
+            })
+        )
+
+        setName("")
+    }
+
+    return(
+        <form>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+            <button type="button" onClick={handleAdd}>
+                add
+            </button>
+        </form>
+    )
+}
+
+export default AddTodo
