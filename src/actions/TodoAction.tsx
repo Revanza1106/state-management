e7b65a17c59e275b4e95f4eca72bfc4a2d345dea
Revanza Firdaus <revanza.firdaus@gmail.com>
2025-08-18 05:05:03 +0700

    feat: Introduce todo action

---
+++ src/actions/TodoAction.tsx
@@ -0,0 +1,11 @@
+import type { TodoItem,CounterAction } from "../interface"
+
+export const addTodo = (todo:TodoItem):CounterAction => ({
+    type:"ADD",
+    payload:todo,
+})
+
+export const deleteTodo = (id:string):CounterAction => ({
+    type:"DELETE",
+    payload:id,
+}) 
